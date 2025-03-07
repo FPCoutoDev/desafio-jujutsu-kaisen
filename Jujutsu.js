b36f44b9f1c6e19e4dacd7bf733aa208e02423e1
FPCoutoDev <contatocoutomiguel@gmail.com>
2025-03-07 15:53:06 -0300

    Adicionei outras classes, agora permitindo além de feiticeiros, uchihas e ninjas. Agora também não é mais possível colocar habilidades de classes diferentes às permitidas, por exemplo, um feiticeiro não pode mais usar nenhuma habilidade de ninjas. Caso seja realizado, o código enviará uma mensagem de erro, e não permitirá continuar. Foi retirado o parametro idade, pois não era utilizada no código, e implementada o parametrô classe. Novas habilidades como susanoo, kotoamatsukami, amaterasu, chidori, rasengan e genjutsu. Támbem foi adicionado uma nova mecanica de dano aleatorio no quesito genjutsu, o dano pode variar de 0 à 100.

--- Jujutsu.js
+++ Jujutsu.js
@@ -1,36 +1,83 @@
-const magiaJujutsu = {
-    INFINITO: {nome: "infinity Void", dano: 999},
-    VESSEL: {nome: "Malevont Shrine", dano: 50},
-    RIKA: {nome: "Authentic Mutual Love", danoMinimo: 20, danoMaximo:80}
+const classes = {
+    FEITICEIRO: "feiticeiro",
+    UCHIHAS: "uchiha",
+    NINJAS: "ninja"
 }
 
-class Feiticeiros{
-    constructor(nome, idade, magia, vida){
-        if (!Object.keys(magiaJujutsu).includes(magia)) {// chat gpt me auxiliou para produzir essa mensagem de erro
-        throw new Error(`Escolha apenas entre INFINITO, VESSEL OU RIKA `);}
+const magiasPermitidas = {
+    feiticeiro: ["INFINITO", "VESSEL", "RIKA"],
+    uchiha: ["AMATERASU", "KOTOAMATSUKAMI", "SUSANOO"],
+    ninja: ["CHIDORI", "RASENGAN", "GENJUTSU"]
+}
+
+const habilidades = {
+    INFINITO: {nome: "infinity Void", dano: 999},
+    VESSEL: {nome: "Malevont Shrine", dano: 50},
+    RIKA: {nome: "Authentic Mutual Love", danoMinimo: 20, danoMaximo:80},
+    CHIDORI: {nome: "Chidori Kirin", dano: 99},
+    RASENGAN: {nome: "Rasen Shuriken", dano: 100},
+    GENJUTSU: {nome: "Genjutsu", dano: 100},
+    AMATERASU: {nome: "Amaterasu eterno", dano: 99},
+    KOTOAMATSUKAMI: {nome: "Kotoamatsukami", danoMinimo: 100, danoMaximo: 999},
+    SUSANOO: {nome: "Susanoo Perfeito", dano: 140}
+
+}
+
+
+class Personagens{
+    constructor(classe, nome, magia, vida){
+    if (!magiasPermitidas[classe].includes(magia)) {
+        throw new Error(`A classe ${classe} não pode usar a magia ${magia}. Escolha entre: ${magiasPermitidas[classe].join(", ")}`);
+    }
+    this.classe = classe;
     this.nome = nome;
-    this.idade = idade;
     this.magia = magia;
     this.vida = vida;
     }
 }
-function ataqueJujutsu(feiticeiro, atacado ){
-        const magiaDamage = magiaJujutsu[feiticeiro.magia]
+function ataquePersonagem(atacantes, atacado ){
+        const magiaDamage = habilidades[atacantes.magia]
         let damage = magiaDamage.dano
 
-    if(feiticeiro.magia === "RIKA"){
-        damage= Math.random() < 0.5 ? magiaDamage.danoMinimo : magiaDamage.danoMaximo;
-}
-    console.log(`${feiticeiro.nome} atacou ${atacado.nome} com ${magiaDamage.nome}, causando ${damage} de pontos de vida.`)
+    if(atacantes.magia === "KOTOAMATSUKAMI"){
+            damage= Math.random() < 0.75 ? magiaDamage.danoMinimo : magiaDamage.danoMaximo;
+        if(damage === magiaDamage.danoMaximo){
+            console.log("DANO CRITICO!")}
+        }
+
+    if(atacantes.magia === "RIKA"){
+            damage= Math.random() < 0.5 ? magiaDamage.danoMinimo : magiaDamage.danoMaximo;
+        if(damage === magiaDamage.danoMaximo){
+            console.log("DANO CRITICO!")}
+        }
+
+    if(atacantes.magia === "GENJUTSU"){
+        damage= Math.random() * magiaDamage.dano
+        if(damage === magiaDamage.danoMaximo){
+            console.log("DANO CRITICO!")}
+        }
+
+    console.log(`O ${atacantes.classe} ${atacantes.nome} atacou ${atacado.nome} com ${magiaDamage.nome}, causando ${damage} de pontos de vida.`)
 atacado.vida -= damage
     if(atacado.vida <= 0){
         atacado.vida = 0
-        console.log(`${atacado.nome} não resistiu e foi abatido por ${feiticeiro.nome}`)}   
+        console.log(`${atacado.nome} não resistiu e foi abatido por ${atacantes.nome}`)}   
     else {console.log(`${atacado.nome} agora tem ${atacado.vida} de vida restante.`) 
    }
 }
-let firstCharacter = new Feiticeiros("Satoro Gojo", 27, "INFINITO", 140)
-let secondCharacter = new Feiticeiros("Sukuna", 500, 'VESSEL', 150 )
-let thirdCharacter = new Feiticeiros("Yuta Okkotsu", 23, "RIKA", 130)
-
-ataqueJujutsu(thirdCharacter, secondCharacter)
+let firstCharacter = new Personagens(classes.FEITICEIRO, "Satoro Gojo", "INFINITO", 140)
+let secondCharacter = new Personagens(classes.FEITICEIRO, "Sukuna", 'VESSEL', 150 )
+let thirdCharacter = new Personagens(classes.FEITICEIRO, "Yuta Okkotsu", "RIKA", 130)
+let fourthCharacter = new Personagens(classes.NINJAS, "Kakashi Hatake", "CHIDORI", 100);
+let fifthCharacter = new Personagens(classes.NINJAS, "Naruto Uzumaki", "RASENGAN", 110);
+let sixthCharacter = new Personagens(classes.NINJAS, "Sakura", "GENJUTSU", 110);
+let seventhCharacter = new Personagens(classes.UCHIHAS, "SASUKE", "AMATERASU", 150);
+let eighthCharacter = new Personagens(classes.UCHIHAS, "SHISUI", "KOTOAMATSUKAMI", 110);
+let ninthCharacter = new Personagens(classes.UCHIHAS, "MADARA", "CHIDORI", 200);
+console.log("//////////////////////////////////////////////////////")
+ataquePersonagem(thirdCharacter, secondCharacter)
+console.log("//////////////////////////////////////////////////////")
+ataquePersonagem(eighthCharacter, ninthCharacter)
+console.log("//////////////////////////////////////////////////////")
+ataquePersonagem(sixthCharacter, ninthCharacter)
+console.log("//////////////////////////////////////////////////////")
